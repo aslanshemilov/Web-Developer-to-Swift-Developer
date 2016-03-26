49db609029f2f11a1ed351e376ae56eeec2ad5bf
Joshua Johanan <josh359@gmail.com>
2016-03-26 19:09:43 -0400

    removed the storyboards and updated AppDelegate
    The project is ready for our code now.

--- Trecco/AppDelegate.swift
+++ Trecco/AppDelegate.swift
@@ -15,7 +15,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        // Override point for customization after application launch.
+        //set the window to the main screen
+        window = UIWindow(frame: UIScreen.mainScreen().bounds)
+        //create new controller
+        let mainViewController = MainViewController()
+        //make it root and visible
+        let navController = UINavigationController(rootViewController: mainViewController)
+        window?.rootViewController = navController
+        window?.makeKeyAndVisible()
         return true
     }
 
