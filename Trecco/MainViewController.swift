e2f36b0b2ccdad327e0c3d45497facaa1aea4ce7
Joshua Johanan <josh359@gmail.com>
2016-05-12 21:10:01 -0400

    Added Trello data objects to MainViewController.

--- Trecco/MainViewController.swift
+++ Trecco/MainViewController.swift
@@ -9,8 +9,12 @@
 import UIKit
 import AVFoundation
 
+//for TrelloAccount
+extension NSUserDefaults: UserDefaultsProtocol {}
+
 class MainViewController: UIViewController, StartViewProtocol {
     var infoLoaded: Bool = false
+    var _trelloAccount: TrelloAccount!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,6 +23,9 @@ class MainViewController: UIViewController, StartViewProtocol {
         self.title = "Trecco"
         self.view = StartView()
         (self.view as! StartView).delegate = self
+        
+        //create trelloAccount
+        _trelloAccount = TrelloAccount(defaultObject: NSUserDefaults.standardUserDefaults())
         
         //ask for record permission
         let avSession: AVAudioSession = AVAudioSession.sharedInstance()
@@ -30,6 +37,21 @@ class MainViewController: UIViewController, StartViewProtocol {
         NSNotificationCenter.defaultCenter().addObserver(self, selector: "trelloAuthFinish:", name: GlobalConstants.TRELLO_AUTH_FINISH, object: nil)
         NSNotificationCenter.defaultCenter().addObserver(self, selector: "trelloLoadInfo:", name: GlobalConstants.TRELLO_LOAD_INFO, object: nil)
     }
+    
+    override func viewDidAppear(animated: Bool) {
+        //check if we are authenticated
+        let trelloConnected: Bool = (_trelloAccount.isTrelloConnected())
+        
+        updateInstructionsAndRecord()
+        
+        if(!trelloConnected) {
+            (self.view as! StartView).createTrelloLoginButton()
+        }else{
+            //if authenticated say we are
+            NSNotificationCenter.defaultCenter().postNotificationName(GlobalConstants.TRELLO_AUTH_FINISH, object: nil)
+        }
+    }
+
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -42,14 +64,93 @@ class MainViewController: UIViewController, StartViewProtocol {
         self.navigationController?.pushViewController(recording, animated: true)
     }
     
-    func trelloOAuthSend(sender: UIButton!) {
+    // MARK: Trello Authentication
+    
+    func trelloAuthFinish(notification: NSNotification) {
+        //check if we have your info
+        
+        if(_trelloAccount.isTrelloConnected() == true && _trelloAccount.getTrelloName() != nil) {
+            NSNotificationCenter.defaultCenter().postNotificationName(GlobalConstants.TRELLO_LOAD_INFO, object: nil)
+            return
+        }
+        
+        
+        let keyOptional = NSBundle.mainBundle().objectForInfoDictionaryKey(GlobalConstants.TRELLO_API_KEY) as? String
+        let tokenOptional = _trelloAccount.getTrelloAccountToken()
+        
+        guard let key = keyOptional else {
+            HandleError.ShowAlert("Could not connect to Trello", viewController: self)
+            return
+        }
+        
+        guard let token = tokenOptional else {
+            HandleError.ShowAlert("Could not connect to Trello", viewController: self)
+            return
+        }
+        
+        let trelloAPI = TrelloAPI(key: key, token: token, http: HTTP())
+        trelloAPI.getMe()
+            .onSuccess{member in
+                self._trelloAccount.setTrelloAvatarHash(member.avatarHash)
+                self._trelloAccount.setTrelloName(member.fullName)
+                NSNotificationCenter.defaultCenter().postNotificationName(GlobalConstants.TRELLO_LOAD_INFO, object: nil)
+            }.onFailure{error in
+                HandleError.ShowError("Failed to get your Trello account info.", error: error, viewController: self)
+        }
+    }
+    
+    func trelloLoadInfo(notification: NSNotification) {
+        if (infoLoaded) {
+            return
+        }
+        
+        createTrelloAuthenticatedView()
+        updateInstructionsAndRecord()
+    }
 
+    
+    func trelloOAuthSend(sender: UIButton!) {
+        let mainBundle = NSBundle.mainBundle()
+        let trelloKeyOp = mainBundle.objectForInfoDictionaryKey(GlobalConstants.TRELLO_API_KEY) as? String
+        let trelloSecretOp = mainBundle.objectForInfoDictionaryKey(GlobalConstants.TRELLO_API_SECRET) as? String
+        
+        guard let trelloKey = trelloKeyOp else {HandleError.ShowAlert("Could not connect to Trello", viewController: self); return}
+        
+        guard let trelloSecret = trelloSecretOp else {HandleError.ShowAlert("Could not connect to Trello", viewController: self); return}
+        
+        let trelloAuth = TrelloOAuth(apiKey: trelloKey, apiSecret: trelloSecret)
+        trelloAuth.authenticate(OauthViewController(),
+            completionHandler: self.handleOAuthReturn)
+    }
+    
+    func handleOAuthReturn(token: String?, error: ErrorType?) {
+        if let error = error {
+            if (shouldErrorFireAnAlert(error)){
+                //one is just OAuth was denied
+                HandleError.ShowError("Error when authorizing your Trello account.", error: error, viewController: self)
+            }
+            
+        }
+        
+        if let token = token {
+            self.didAuthenticate(token)
+        }
+    }
+    
+    func shouldErrorFireAnAlert(error: ErrorType) -> Bool {
+        print((error as NSError))
+        return (error as NSError).code != -1
+    }
+    
+    func didAuthenticate(token: String) {
+        _trelloAccount.setTrelloAccountToken(token)
+        NSNotificationCenter.defaultCenter().postNotificationName(GlobalConstants.TRELLO_AUTH_FINISH, object: nil)
     }
     
     func logOutOfTrello() {
         let alert = UIAlertController(title: "Trello Log Out", message: "Do you want to log out of Trello?", preferredStyle: .Alert)
         let yesAction = UIAlertAction(title: "Yes", style: .Default, handler: {action in
-            //self._trelloAccount.removeTrello()
+            self._trelloAccount.removeTrello()
             
             (self.view as! StartView).createTrelloLoginButton()
             
@@ -65,6 +166,29 @@ class MainViewController: UIViewController, StartViewProtocol {
         alert.addAction(noAction)
         
         self.presentViewController(alert, animated: true, completion: nil)
+    }
+    
+    func createTrelloAuthenticatedView() {
+        let hashOp = _trelloAccount.getTrelloAvatarHashURL("https://trello-avatars.s3.amazonaws.com/")
+        let nameOp = self._trelloAccount.getTrelloName()
+        
+        guard let hash = hashOp else {return}
+        guard let name = nameOp else {return}
+        
+        let imageURLOp = NSURL(string: hash)
+        
+        guard let imageURL = imageURLOp else {return}
+        
+        dispatch_async(dispatch_get_global_queue(QOS_CLASS_USER_INITIATED, 0)){
+            let image_data = NSData(contentsOfURL: imageURL)
+            
+            dispatch_async(dispatch_get_main_queue()) {
+                (self.view as! StartView).createTrelloAuthenticatedView(image_data, name: name)
+            }
+        }
+        
+        
+        self.infoLoaded = true
     }
     
     func createWarningAttributedString(trelloConnected: Bool, recordPermission: Bool) -> NSMutableAttributedString {
@@ -97,7 +221,7 @@ class MainViewController: UIViewController, StartViewProtocol {
     func updateInstructionsAndRecord() {
         let avSession: AVAudioSession = AVAudioSession.sharedInstance()
         let recordPermission: Bool = avSession.recordPermission() == AVAudioSessionRecordPermission.Granted
-        let trelloConnected: Bool = false
+        let trelloConnected: Bool = (_trelloAccount.isTrelloConnected())
         
         self.setInstructions(trelloConnected, recordPermission: recordPermission)
     }
@@ -105,6 +229,10 @@ class MainViewController: UIViewController, StartViewProtocol {
     func setInstructions(trelloConnected: Bool, recordPermission: Bool) {
         let attributedString = self.createWarningAttributedString(trelloConnected, recordPermission: recordPermission)
         (self.view as! StartView).updateInstructions(attributedString)
+    }
+    
+    deinit {
+        NSNotificationCenter.defaultCenter().removeObserver(self)
     }
 
 }
